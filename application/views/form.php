03d7633757657d9a2c2acebb8c42be6c7858d24c
Nislacker <nislacker@gmail.com>
2019-07-07 22:26:15 +0300

    Добавил автоматическую транслитерацию через JS для readonly-поля 'File name', улучшенный код которой взят со страницы http://slivsol.blogspot.com/2014/03/translit-generator-url-dlya-sajtov-onlajn.html

--- application/views/form.php
+++ application/views/form.php
@@ -26,6 +26,7 @@
         <div class="col-sm-10">
             <input type="text" class="form-control"
                    id="fileName" name="fileName"
+                   readonly
                    placeholder="registratsiya-lekarstvennyh-sredstv-v-ukraine-registratsiya-lekarstv"
                    size="75" maxlength="256"
                    value="<?= isset($_POST['fileName']) ? $_POST['fileName'] : '' ?>">
@@ -38,7 +39,7 @@
             <input type="text" class="form-control"
                    id="title" name="title"
                    placeholder="Регистрация лекарственных средств в Украине, регистрация лекарств"
-                   size="75" maxlength="100"
+                   size="75" maxlength="200"
                    value="<?= isset($_POST['title']) ? $_POST['title'] : '' ?>">
         </div>
     </div>
@@ -59,7 +60,7 @@
             <input type="text" class="form-control"
                    id="h1" name="h1"
                    placeholder="Государственная регистрация лекарственных средств в Украине"
-                   size="60" maxlength="100"
+                   size="60" maxlength="200"
                    value="<?= isset($_POST['h1']) ? $_POST['h1'] : '' ?>">
         </div>
     </div>
@@ -114,48 +115,182 @@
 
 <script>
 
-    document.getElementById('createArticlesFromDB').onclick = function () {
-        if ((document.getElementById('fileName').value !== '') ||
-            (document.getElementById('title').value !== '') ||
-            (document.getElementById('description').value !== '') ||
-            (document.getElementById('h1').value !== '') ||
-            (document.getElementById('text').value !== '')) {
+    document.getElementById('title').oninput = function () {
+            let text = document.getElementById('title').value;
+            let transl = [];
 
-            if (confirm('Form is not empty. Are you sure to go creating article(s) from DB?')) {
+            // укр. алфавит
+            transl['Ґ'] = 'g';
+            transl['Є'] = 'Ie';
+            transl['є'] = 'a';
+            transl['І'] = 'I';
+            transl['і'] = 'i';
+            transl['Ї'] = 'I';
+            transl['ї'] = 'i';
+
+            // рус. алфавит
+            transl['А'] = 'a';
+            transl['а'] = 'a';
+            transl['Б'] = 'b';
+            transl['б'] = 'b';
+            transl['В'] = 'v';
+            transl['в'] = 'v';
+            transl['Г'] = 'g';
+            transl['г'] = 'g';
+            transl['Д'] = 'd';
+            transl['д'] = 'd';
+            transl['Е'] = 'e';
+            transl['е'] = 'e';
+            transl['Ё'] = 'yo';
+            transl['ё'] = 'yo';
+            transl['Ж'] = 'zh';
+            transl['ж'] = 'zh';
+            transl['З'] = 'z';
+            transl['з'] = 'z';
+            transl['И'] = 'i';
+            transl['и'] = 'i';
+            transl['Й'] = 'j';
+            transl['й'] = 'j';
+            transl['К'] = 'k';
+            transl['к'] = 'k';
+            transl['Л'] = 'l';
+            transl['л'] = 'l';
+            transl['М'] = 'm';
+            transl['м'] = 'm';
+            transl['Н'] = 'n';
+            transl['н'] = 'n';
+            transl['О'] = 'o';
+            transl['о'] = 'o';
+            transl['П'] = 'p';
+            transl['п'] = 'p';
+            transl['Р'] = 'r';
+            transl['р'] = 'r';
+            transl['С'] = 's';
+            transl['с'] = 's';
+            transl['Т'] = 't';
+            transl['т'] = 't';
+            transl['У'] = 'u';
+            transl['у'] = 'u';
+            transl['Ф'] = 'f';
+            transl['ф'] = 'f';
+            transl['Х'] = 'h';
+            transl['х'] = 'h';
+            transl['Ц'] = 'ts';
+            transl['ц'] = 'ts';
+            transl['Ч'] = 'ch';
+            transl['ч'] = 'ch';
+            transl['Ш'] = 'sh';
+            transl['ш'] = 'sh';
+            transl['Щ'] = 'sch';
+            transl['щ'] = 'sch';
+            transl['Ъ'] = '';
+            transl['ъ'] = '';
+            transl['Ь'] = '';
+            transl['ь'] = '';
+            transl['Ы'] = 'y';
+            transl['ы'] = 'y';
+            transl['Э'] = 'e';
+            transl['э'] = 'e';
+            transl['Ю'] = 'yu';
+            transl['ю'] = 'yu';
+            transl['Я'] = 'ya';
+            transl['я'] = 'ya';
+            transl[' '] = '-';
+
+            transl['A'] = 'a';
+            transl['N'] = 'n';
+            transl['B'] = 'b';
+            transl['O'] = 'o';
+            transl['C'] = 'c';
+            transl['P'] = 'p';
+            transl['D'] = 'd';
+            transl['Q'] = 'q';
+            transl['E'] = 'e';
+            transl['R'] = 'r';
+            transl['F'] = 'f';
+            transl['S'] = 's';
+            transl['G'] = 'g';
+            transl['T'] = 't';
+            transl['H'] = 'h';
+            transl['U'] = 'u';
+            transl['I'] = 'i';
+            transl['V'] = 'v';
+            transl['J'] = 'j';
+            transl['W'] = 'w';
+            transl['K'] = 'k';
+            transl['X'] = 'x';
+            transl['L'] = 'l';
+            transl['Y'] = 'y';
+            transl['M'] = 'm';
+            transl['Z'] = 'z';
+
+            transl['.'] = '';
+            transl[','] = '';
+            transl['!'] = '';
+            transl['?'] = '';
+            transl[';'] = '';
+            transl[':'] = '';
+            transl['/'] = '';
+            transl['_'] = '';
+            transl['('] = '';
+            transl[')'] = '';
+            transl['-'] = '-';
+            transl['"'] = '';
+
+            let result = '';
+            for (let i = 0; i < text.length; i++) {
+                if (transl[text[i]] !== undefined) {
+                    result += transl[text[i]];
+                }
+                else {
+                    result += text[i];
+                }
+            }
+            document.getElementById('fileName').value = result;
+        };
+
+        document.getElementById('createArticlesFromDB').onclick = function () {
+            if ((document.getElementById('fileName').value !== '') ||
+                (document.getElementById('title').value !== '') ||
+                (document.getElementById('description').value !== '') ||
+                (document.getElementById('h1').value !== '') ||
+                (document.getElementById('text').value !== '')) {
+
+                if (confirm('Form is not empty. Are you sure to go creating article(s) from DB?')) {
+                    window.location.href = window.location.origin + '/create';
+                    // window.open(window.location.origin + '/create', '_self');
+                }
+            }
+            else {
                 window.location.href = window.location.origin + '/create';
                 // window.open(window.location.origin + '/create', '_self');
             }
-        }
-        else {
-            window.location.href = window.location.origin + '/create';
-            // window.open(window.location.origin + '/create', '_self');
-        }
-    };
-
-    document.getElementById('reset').onclick =
-        function () {
-            document.getElementById('fileName').setAttribute('value', '');
-            document.getElementById('title').setAttribute('value', '');
-            document.getElementById('description').innerText = '';
-            document.getElementById('h1').setAttribute('value', '');
-            document.getElementById('text').innerText = '';
         };
 
-    document.getElementById('showPlaceholders').onchange =
-        function () {
-            if (document.getElementById('showPlaceholders').checked === true) {
-                document.getElementById('fileName').setAttribute('placeholder', 'registratsiya-lekarstvennyh-sredstv-v-ukraine-registratsiya-lekarstv');
-                document.getElementById('title').setAttribute('placeholder', 'Регистрация лекарственных средств в Украине, регистрация лекарств');
-                document.getElementById('description').setAttribute('placeholder', 'В соответствии с Законом Украины «О лекарственных средствах» обращение лекарственных средств на территории Украины возможно только после прохождения процедуры государственной регистрации.');
-                document.getElementById('h1').setAttribute('placeholder', 'Государственная регистрация лекарственных средств в Украине');
-                document.getElementById('text').setAttribute('placeholder', '');
+        document.getElementById('reset').onclick =
+            function () {
+                document.getElementById('fileName').setAttribute('value', '');
+                document.getElementById('title').setAttribute('value', '');
+                document.getElementById('description').innerText = '';
+                document.getElementById('h1').setAttribute('value', '');
+                document.getElementById('text').innerText = '';
+            };
+
+        document.getElementById('showPlaceholders').onchange =
+            function () {
+                if (document.getElementById('showPlaceholders').checked === true) {
+                    document.getElementById('fileName').setAttribute('placeholder', 'registratsiya-lekarstvennyh-sredstv-v-ukraine-registratsiya-lekarstv');
+                    document.getElementById('title').setAttribute('placeholder', 'Регистрация лекарственных средств в Украине, регистрация лекарств');
+                    document.getElementById('description').setAttribute('placeholder', 'В соответствии с Законом Украины «О лекарственных средствах» обращение лекарственных средств на территории Украины возможно только после прохождения процедуры государственной регистрации.');
+                    document.getElementById('h1').setAttribute('placeholder', 'Государственная регистрация лекарственных средств в Украине');
+                    document.getElementById('text').setAttribute('placeholder', '');
+                }
+                else {
+                    document.getElementById('fileName').setAttribute('placeholder', '');
+                    document.getElementById('title').setAttribute('placeholder', '');
+                    document.getElementById('description').setAttribute('placeholder', '');
+                    document.getElementById('h1').setAttribute('placeholder', '');
+                    document.getElementById('text').setAttribute('placeholder', '');
+                }
             }
-            else {
-                document.getElementById('fileName').setAttribute('placeholder', '');
-                document.getElementById('title').setAttribute('placeholder', '');
-                document.getElementById('description').setAttribute('placeholder', '');
-                document.getElementById('h1').setAttribute('placeholder', '');
-                document.getElementById('text').setAttribute('placeholder', '');
-            }
-        }
 </script>
